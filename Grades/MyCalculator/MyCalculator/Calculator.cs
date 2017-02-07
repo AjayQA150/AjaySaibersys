dee8822516f7bffb3c37620b32ae08b14468c5fe
AjayQA150 <ajayqa150@gmail.com>
2017-02-06 19:19:50 -0600

    Add files via upload

---
+++ Grades/MyCalculator/MyCalculator/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculator
+{
+    public class Calculator
+    {
+        public int add(int first,int second)
+        {
+            return first + second;
+        }
+        public int divide(int dividend,int divisor)
+        {
+            return dividend/divisor;
+        }
+        public int test(int dividend, int divisor)
+        {
+            int result = 0;
+            try
+            {
+                Calculator calcuator = new Calculator();
+                result = calcuator.divide(dividend, divisor);
+                
+            }
+            catch (DivideByZeroException)
+            {
+                result = int.MaxValue;
+            }
+            catch(Exception)
+            {
+                result = int.MinValue;
+            }
+            return result;
+        }
+    }
+}
---
+++ Grades/MyCalculator/UnitTestProject1/CalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCalculator;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        [TestCategory("Add")]
+        [Priority(0)]
+        public void AddSimple()
+        {
+            var calculator = new Calculator();
+            int sum=calculator.add(1,2);
+            Assert.AreEqual(3, sum);
+        }
+        [TestMethod]
+        [TestCategory("Divide")]
+        [Priority(1)]
+       // [Ignore]
+        public void DivideSimple()
+        {
+            var calculator = new Calculator();
+            int quetient = calculator.divide(10, 5);
+            Assert.AreEqual(2, quetient);
+        }
+        [TestMethod]
+        [TestCategory("Divide")]
+        [ExpectedException(typeof(DivideByZeroException))]
+        [Priority(1)]
+        public void DividebyZero()
+        {
+            var calculator = new Calculator();
+             calculator.divide(10, 0);
+           
+        }
+
+
+    }
+}
